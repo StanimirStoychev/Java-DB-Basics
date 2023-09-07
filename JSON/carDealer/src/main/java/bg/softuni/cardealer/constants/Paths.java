5c49745cd0663bb0117509470a97a35650fcfc7e
Stanimir <stmbksa@gmail.com>
2023-09-07 12:28:27 +0300

    insert new files and paths

--- JSON/carDealer/src/main/java/bg/softuni/cardealer/constants/Paths.java
+++ JSON/carDealer/src/main/java/bg/softuni/cardealer/constants/Paths.java
@@ -5,21 +5,47 @@ import java.nio.file.Path;
 public enum Paths {
     ;
 
-    public static final Path SUPPLIERS_INPUT_PATH =
-            Path.of("src", "main", "resources", "files", "suppliers.json");
-    public static final Path PARTS_INPUT_PATH =
-            Path.of("src", "main", "resources", "files", "parts.json");
-    public static final Path CAR_INPUT_PATH =
-            Path.of("src", "main", "resources", "files", "cars.json");
-    public static final Path CUSTOMER_INPUT_PATH =
-            Path.of("src", "main", "resources", "files", "customers.json");
+    //Inputs
 
+    //JSON
+    public static final Path SUPPLIERS_INPUT_PATH =
+            Path.of("src", "main", "resources", "files", "json", "suppliers.json");
+    public static final Path PARTS_INPUT_PATH =
+            Path.of("src", "main", "resources", "files", "json", "parts.json");
+    public static final Path CAR_INPUT_PATH =
+            Path.of("src", "main", "resources", "files", "json", "cars.json");
+    public static final Path CUSTOMER_INPUT_PATH =
+            Path.of("src", "main", "resources", "files", "json", "customers.json");
+
+    //XML
+    public static final Path SUPPLIERS_INPUT_XML_PATH =
+            Path.of("src", "main", "resources", "files", "xml", "suppliers.xml");
+    public static final Path PARTS_INPUT_XML_PATH =
+            Path.of("src", "main", "resources", "files", "xml", "parts.xml");
+    public static final Path CAR_INPUT_XML_PATH =
+            Path.of("src", "main", "resources", "files", "xml", "cars.xml");
+    public static final Path CUSTOMER_INPUT_XML_PATH =
+            Path.of("src", "main", "resources", "files", "xml", "customers.xml");
+
+    //Outputs
+
+    //JSON
     public static final Path ORDERED_CUSTOMERS_OUTPUT =
-            Path.of("src", "main", "resources", "output", "ordered-customers.json");
+            Path.of("src", "main", "resources", "output", "json", "ordered-customers.json");
     public static final Path TOYOTA_CARS_OUTPUT =
-            Path.of("src", "main", "resources", "output", "toyota-cars.json");
+            Path.of("src", "main", "resources", "output", "json", "toyota-cars.json");
     public static final Path LOCAL_SUPPLIERS_OUTPUT =
-            Path.of("src", "main", "resources", "output", "local-suppliers.json");
+            Path.of("src", "main", "resources", "output", "json", "local-suppliers.json");
     public static final Path CARS_AND_PARTS_OUTPUT =
-            Path.of("src", "main", "resources", "output", "cars-and-parts.json");
+            Path.of("src", "main", "resources", "output", "json", "cars-and-parts.json");
+
+    //XML
+    public static final Path ORDERED_CUSTOMERS_XML_OUTPUT =
+            Path.of("src", "main", "resources", "output", "xml", "ordered-customers.xml");
+    public static final Path TOYOTA_CARS_XML_OUTPUT =
+            Path.of("src", "main", "resources", "output", "xml", "toyota-cars.xml");
+    public static final Path LOCAL_SUPPLIERS_XML_OUTPUT =
+            Path.of("src", "main", "resources", "output", "xml", "local-suppliers.xml");
+    public static final Path CARS_AND_PARTS_XML_OUTPUT =
+            Path.of("src", "main", "resources", "output", "xml", "cars-and-parts.xml");
 }
